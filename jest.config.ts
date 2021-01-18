3bd69a6d2594a964c1bd16123008b153552336b6
Josep Seto <jsetojseto@gmail.com>
2021-01-18 01:23:22 +0100

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+const pathsToIgnore = [
+	"/node_modules/",
+	"/dist/",
+	"/out/",
+	"/coverage/"
+];
+
+module.exports = {
+	globals: {
+		'ts-jest': { // configuramos para que Jest use typescript
+			'tsConfig': 'tsconfig.json', // usar la configuración estandart de TS
+		}
+	},
+	transform: { // usar TS para extensiones de fichero ts i tsx
+		"^.+\\.(ts|tsx)$": "ts-jest"
+	},
+	testMatch: [ // ejecutar tests en los ficheros que contengan ".spec" justo antes de extensiones ts, tsx, js, jsx y se encuentren en una carpeta test
+		"**/*.spec.+(ts|tsx|js|jsx)"
+	],
+	watchPathIgnorePatterns: pathsToIgnore, // no ejectar tests cuando haya cambios en los directorios de pathToIgnore
+	testPathIgnorePatterns: pathsToIgnore, // no incluir los directorios de pathToIgnore al ejecutar los tests
+	// setupFilesAfterEnv: [
+	// 	"<rootDir>/jest-addons/setup-tests.ts"
+	// ],
+}
